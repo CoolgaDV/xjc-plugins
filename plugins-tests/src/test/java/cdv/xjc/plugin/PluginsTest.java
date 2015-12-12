a3c4d9bffe728d528163eef54cf4e382f6d79264
coolga <coolgadv@gmail.com>
2015-12-12 16:09:09 +0300

    Generated constructor test

--- plugins-tests/src/test/java/cdv/xjc/plugin/PluginsTest.java
+++ plugins-tests/src/test/java/cdv/xjc/plugin/PluginsTest.java
@@ -13,7 +13,15 @@ import plugin.xjc.cdv.test.Person;
 public class PluginsTest {
 
     @Test
-    public void testToSting() throws Exception {
+    public void testConstructor() {
+        Person person = new Person("foo", 42, "bar");
+        Assert.assertEquals("foo", person.getName());
+        Assert.assertEquals(42, person.getAge());
+        Assert.assertEquals("bar", person.getAddress());
+    }
+
+    @Test
+    public void testToSting() {
         Person person = new Person();
         person.setName("foo");
         person.setAge(42);
